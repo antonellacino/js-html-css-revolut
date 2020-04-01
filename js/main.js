0733e281c771e0c3dd73a402b5eb88b4add392bb
Antonella Cino <antonella.cino18@gmail.com>
2020-04-02 01:58:50 +0200

    responsive ok

--- js/main.js
+++ js/main.js
@@ -2,13 +2,33 @@ $(document).ready(function(){
   //imposto variabile di stato
   var stateItem = "";
   //salvo in una varibile la selezione dei tag a del hamburger menu
-  var listItem = $('div.hamburger-menu a');
+  var listItem = $('div.nav-dx a');
+  var hamburgerMenu = $('.nav-dx');
 
   listItem.click(
     function () {
-      //seleziono il div fratello del tag a
       $(this).siblings('div').toggle();
+  })
 
-    })
+  $('.fa-bars').click(
+    function(){
+      $(this).hide();
+      $('.hamburger-menu').show();
+      $('.fa-times').show();
+      $('.nav-sx h1').addClass('fadeOut');
+    }
+  )
+
+  $('.fa-times').click(
+    function(){
+      $(this).hide();
+      $('.hamburger-menu').hide();
+      $('.fa-bars').show();
+      $('.nav-sx h1').removeClass('fadeOut');
+      $('.nav-sx h1').addClass('fadeIn');
+    }
+  )
+
+
 
 });
